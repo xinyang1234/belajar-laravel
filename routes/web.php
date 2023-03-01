661e4082d7be1c0e276cec5ff88d22988971689b
Muhammad Sulaiman <91898723+xinyang1234@users.noreply.github.com>
2023-03-01 10:30:23 +0700

    minggu4

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Frontend\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//  Route::get('/hi',  function() {
+//     return 'Hello World';
+//  });
+
+// Route::group(['namespace' => 'Frontend'], function() 
+// {
+//     Route::resource('home', 'HomeController');
+// });
+
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get("/home", function() {
+//     return "hh";
+// });
+
+route::get('home', [HomeController::class, 'index']);
+
